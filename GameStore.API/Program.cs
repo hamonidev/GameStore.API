34421d4f0711954579320d3fc3519258959db90d
Gui Hamoni <gui.dev2004@gmail.com>
2026-08-15 20:46:22 +0100

    Handle non-existing resources

--- GameStore.API/Program.cs
+++ GameStore.API/Program.cs
@@ -31,8 +31,13 @@ app.MapGet("/games", () => games);
 
 
 // GET /games/{id}
-app.MapGet("/games/{id}", (int id) => games.Find(game => game.Id == id))
-    .WithName(GetGameEndpoint);
+app.MapGet("/games/{id}", (int id) =>
+{
+    var game = games.Find(game => game.Id == id);
+
+    return game is null ? Results.NotFound() : Results.Ok(game);
+})
+.WithName(GetGameEndpoint);
 
 // POST /games
 app.MapPost("/games", (CreateGameDto newGame) =>
@@ -54,6 +59,11 @@ app.MapPost("/games", (CreateGameDto newGame) =>
 app.MapPut("/games/{id}", (int id, UpdateGameDto updatedGame) =>
 {
     var index = games.FindIndex(game => game.Id == id);
+
+    if (index == -1)
+    {
+        return Results.NotFound();
+    }
 
     games[index] = new GameDto(
         id,
