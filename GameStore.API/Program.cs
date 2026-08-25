c0c46ad1958e2410ddb0b787c3824b8bfa5ad5de
Gui Hamoni <gui.dev2004@gmail.com>
2026-08-25 21:59:31 +0100

    Integrate EF Core

--- GameStore.API/Program.cs
+++ GameStore.API/Program.cs
@@ -1,8 +1,12 @@
+using GameStore.API.Data;
 using GameStore.API.Endpoints;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddValidation();
+
+var connString = "Data Source=GameStore.db";
+builder.Services.AddSqlite<GameStoreContext>(connString);
 
 var app = builder.Build();
 
